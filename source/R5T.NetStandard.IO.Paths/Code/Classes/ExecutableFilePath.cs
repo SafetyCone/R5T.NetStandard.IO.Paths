587542f25804374e6a71fd8e21f7f8044f7bbc69
MinexAutomation <minex.automation@gmail.com>
2019-05-13 23:35:45 -0400

    Added ExecutableFilePath type. Added folder publish profile. Set initial version to 0.0.1.

---
+++ source/R5T.NetStandard.IO.Paths/Code/Classes/ExecutableFilePath.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace R5T.NetStandard.IO.Paths
+{
+    public class ExecutableFilePath : FilePath
+    {
+        public ExecutableFilePath()
+        {
+        }
+
+        public ExecutableFilePath(string value)
+            : base(value)
+        {
+        }
+    }
+}
